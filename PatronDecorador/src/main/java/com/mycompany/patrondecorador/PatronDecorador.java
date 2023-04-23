16d952ef986f40476bfe44f79a1fa41b110a675c
MXCXXSs <nicolas.macias1107@gmail.com>
2023-04-23 08:57:09 -0500

    23/04/2023 	Creación repositorio tarea Patrón Decorador Biblioteca

---
+++ PatronDecorador/src/main/java/com/mycompany/patrondecorador/PatronDecorador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patrondecorador;
+
+/**
+ *
+ * @author nicol
+ */
+public class PatronDecorador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
